ad58491d82e09b145f00e8e82a229bc37cb92e10
ikamrava <kamrava.iman@outlook.com>
2023-08-25 13:48:57 +0100

    before main page

--- component/CustomButton.tsx
+++ component/CustomButton.tsx
@@ -1,9 +1,9 @@
 "use client"
 import { CustomButtonProps } from "@/Types"
 
-function CustomButton({title,containerStyles,handleClick}:CustomButtonProps) {
+function CustomButton({title,containerStyles,handleClick,btnType}:CustomButtonProps) {
   return (
-    <button disabled={false} type={"button"} className={`custom-btn  ${containerStyles}`} onClick={()=>{}}>
+    <button disabled={false} type={btnType} className={`custom-btn  ${containerStyles}`} onClick={()=>{}}>
         <span className={"flex-1"}>{title}</span>
          </button>
   )
--- component/Hero.tsx
+++ component/Hero.tsx
@@ -25,8 +25,9 @@ function Hero() {
         <div className="hero__image-container">
             <div className="hero__image">
                 <Image src="/hero.png" alt="Hero Image" layout="fill" objectFit="contain" />
+                </div>
                 <div className="hero__image-overlay"></div>
-            </div>
+            
         </div>
     </div>
   )
