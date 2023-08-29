dec3b5b770869602e1fbb718b00d4eee5d08e378
ikamrava <kamrava.iman@outlook.com>
2023-08-29 18:27:43 +0100

    prepare for deploy

--- component/SearchMan.tsx
+++ component/SearchMan.tsx
@@ -1,7 +1,6 @@
 "use client"
 import { SearchManuProps } from "@/Types"
 import { Combobox,Transition } from "@headlessui/react"
-import { set } from "firebase/database"
 import Image from "next/image"
 import { Fragment, useState } from "react"
 import {manufacturers} from "../constants/index"
